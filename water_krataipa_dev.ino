07e849f4fde454292c2f2382af8b0e5aad90c6fd
pacharapol4066 <pacharapol4066@gmail.com>
2018-08-12 23:25:26 +0700

    2018-08-12 (REV02)

--- water_krataipa_dev.ino
+++ water_krataipa_dev.ino
@@ -2,26 +2,118 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <SimpleTimer.h>
 #include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
 
-char auth[] = "YourAuthToken";
+#define DHTPIN 5
+#define DHTTYPE DHT11
+
+char auth[] = "7973178c19e44ba3a99b19400be53f70";
 const char* ssid     = "krataipa";
 const char* password = "wildrabbit";
+int pumpFlage1 = 0;
+int pumpFlage2 = 0;
+int pumpFlage3 = 0;
+int pumpFlage4 = 0;
+int pumpFlage5 = 0;
+int pumpFlage6 = 0;
 
-const int relayInput1 = 4;  /* GPI00 (D2)*/
-const int relayInput2 = 0;  /* GPI00 (D3)*/
-const int relayInput3 = 2;  /* GPI02 (D4)*/
-const int relayInput4 = 14;  /* GPI014 (D5)*/
-const int relayInput5 = 12;  /* GPI012 (D6)*/
-const int relayInput6 = 13;  /* GPI013 (D7)*/
-const int relayInput7 = 15;  /* GPI015 (D8)*/
+const int relayInput1 = 4;   /* GP4(D2)*/
+const int relayInput2 = 0;   /* GP0(D3)*/
+const int relayInput3 = 2;   /* GP2(D4)*/
+const int relayInput4 = 14;  /* GP14(D5)*/
+const int relayInput5 = 12;  /* GP12(D6)*/
+const int relayInput6 = 13;  /* GP13(D7)*/
+const int relayInput7 = 15;  /* GP15(D8)*/
 
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+BLYNK_CONNECTED() {
+    Blynk.syncAll();
+}
+
+BLYNK_WRITE(GP4) //Button Widget is writing to pin GP4
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage1 = 1;
+  else if (pinData == 0)
+    pumpFlage1 = 0;
+}
+
+BLYNK_WRITE(GP0) //Button Widget is writing to pin GP0
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage2 = 1;
+  else if (pinData == 0)
+    pumpFlage2 = 0;
+}
+
+BLYNK_WRITE(GP2) //Button Widget is writing to pin GP2
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage3 = 1;
+  else if (pinData == 0)
+    pumpFlage3 = 0;
+}
+
+BLYNK_WRITE(GP14) //Button Widget is writing to pin GP2
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage4 = 1;
+  else if (pinData == 0)
+    pumpFlage4 = 0;
+}
+
+BLYNK_WRITE(GP12) //Button Widget is writing to pin GP2
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage5 = 1;
+  else if (pinData == 0)
+    pumpFlage5 = 0;
+}
+
+BLYNK_WRITE(GP13) //Button Widget is writing to pin GP2
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+    pumpFlage6 = 1;
+  else if (pinData == 0)
+    pumpFlage6 = 0;
+}
+
+BLYNK_WRITE(GP15) //Button Widget is writing to pin GP2
+{
+  int pinData = param.asInt(); 
+  if (pinData == 1) 
+  {
+    if(pumpFlage1 == 1 || pumpFlage2 == 1 || pumpFlage3 == 1 || pumpFlage4 == 1 || pumpFlage5 == 1 || pumpFlage6 == 1){
+        digitalWrite(relayInput7, LOW);
+    }
+  }
+}
+
+void sendSensor()
+{
+  float t = dht.readTemperature();
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Blynk.virtualWrite(V1, t);
+}
 
 void setup() {
   // put your setup code here, to run once:
    Serial.begin(115200);
-   Wire.begin(D0, D1);
+   Wire.begin(D0, D1);    //SCL,SDA
    
    pinMode(relayInput1,OUTPUT);
    pinMode(relayInput2,OUTPUT);
@@ -29,6 +121,7 @@ void setup() {
    pinMode(relayInput4,OUTPUT);
    pinMode(relayInput5,OUTPUT);
    pinMode(relayInput6,OUTPUT);
+   pinMode(relayInput7,OUTPUT);
    
    lcd.begin();
    lcd.backlight();
@@ -42,10 +135,12 @@ void setup() {
    }
    recheckWifi();
    Blynk.begin(auth, ssid, password);
+   timer.setInterval(1000L, sendSensor);
 }
 
 void loop() {
   Blynk.run();
+  timer.run();
 }
 
 void recheckWifi(){
